4f2dafc08d409d0562679a046109b24a238f4e8b
Dipongkar Roy <dipongkarroy233@gmail.com>
2024-06-02 17:14:27 +0600

    convart to limiting name,email done

--- src/module/student/student.service.ts
+++ src/module/student/student.service.ts
@@ -3,7 +3,7 @@ import { Student } from './student.model';
 import { startSession } from 'mongoose';
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
-  console.log('base query ',query);
+  //console.log('base query ',query);
   const queryObj ={...query}
   //{email:{$regex:query.searchTerm ,$options:'i'}}
 
@@ -19,10 +19,10 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
     })),
   });
 //fillter ing working :
-const excludeFields = ['searchTerm' ,'sort','limit']
+const excludeFields = ['searchTerm' ,'sort','limit','page','fields']
 
 excludeFields.forEach(element =>delete queryObj[element])
- console.log({query ,queryObj});
+ console.log({query },{queryObj});
 
 
 const fillterQuery = searchQuery.find(queryObj)
@@ -34,15 +34,31 @@ if(query.sort){
 }
 
 const sortQuery = fillterQuery.sort(sort)
-
+  let page = 1
   let limit = 1 ;
+  let skip = 0
+
   if(query.limit){
-    limit = query.limit ;
+    limit = Number(query.limit) ;
+    console.log(limit);
   }
 
 
-  const limitQuery = await sortQuery.limit(limit)
-  return limitQuery;
+  if(query.page){
+    page = Number(query.page) ;
+    skip = (page-1)*limit
+  }
+
+  const paginateQuery = sortQuery.skip(skip)
+  const limitQuery = paginateQuery.limit(limit)
+//convart fild limiting
+  let fields = '-__v' ;
+  if(query.fields){
+    fields = (query.fields as string).split(',').join(' ')
+    console.log(fields);
+  }
+  const fieldsQuery = await limitQuery.select(fields)
+  return fieldsQuery;
 };
 
 
